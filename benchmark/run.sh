b159705375a21a38851ae9d47ee5576fee5e31ea
Géry Debongnie <ged@odoo.com>
2021-08-30 08:07:04 +0200

    chore: remove benchmark files
    They are now available as a PR on the js framework benchmark project.

--- benchmark/run.sh
+++
@@ -1,9 +0,0 @@
-
-echo "*** Building blockdom ****************************************************"
-npm run build
-
-echo "*** Copying files ********************************************************"
-mkdir -p ../js-framework-benchmark/frameworks/keyed/blockdom
-cp -r benchmark/* ../js-framework-benchmark/frameworks/keyed/blockdom
-cp dist/blockdom.iife.js ../js-framework-benchmark/frameworks/keyed/blockdom/blockdom.js
-
